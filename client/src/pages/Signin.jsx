29e7629f016c6d738833bc7587102bd7ccb20acb
Biniyam-Biyadge <biniyambiyadge@gmail.com>
2025-03-20 18:26:48 +0300

    complete the signup and signin page UI

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 export default function SignIn() {
   return (
-    <div>
-      <h1>Sign in</h1>
+    <div className="body">
+      <div class="container">
+        <h1>Sign In</h1>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Enter your email" />
+
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+
+        <button class="signup-btn">Sign in</button>
+        <p>
+          Dont have an account? <a href="/sign-up">Signup</a>
+        </p>
+      </div>
     </div>
   );
 }
--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -2,8 +2,34 @@ import React from "react";
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>signup</h1>
+    <div className="body">
+      <div class="container">
+        <h1>Sign Up</h1>
+        <label for="name">Name</label>
+        <input type="text" id="name" placeholder="Enter your name" />
+
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Enter your email" />
+
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+
+        <label for="confirm-password">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm-password"
+          placeholder="Enter your password"
+        />
+
+        <button class="signup-btn">Sign Up</button>
+        <p>
+          Already have an account? <a href="/sign-in">Login</a>
+        </p>
+      </div>
     </div>
   );
 }
